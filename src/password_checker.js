61af4c242f98716263db656d8a6f73dfe31c10c9
SizweSeize <mpontshanesizwe@gmail.com>
2020-05-20 22:16:25 +0200

    password

---
+++ src/password_checker.js
@@ -0,0 +1,60 @@
+const longerThanEightChar = /.{9,}/;
+const lowercaseLetter = /[a-z]/g;
+const uppercaseLetter = /[A-Z]/g;
+const digit = /[0-9]/g;
+const specialChar = /\W/g;
+
+function passwordIsValid(password) {
+  if (password == "") {
+    throw Error("password should exist");
+  }
+  if (!password.match(longerThanEightChar)) {
+    throw Error("password length should be longer than 8 characters");
+  }
+  if (!password.match(lowercaseLetter)) {
+    throw Error("password should have at least one lowercase letter");
+  }
+  if (!password.match(uppercaseLetter)) {
+    throw Error("password should have at least one uppercase letter");
+  }
+  if (!password.match(digit)) {
+    throw Error("password should at least have one digit");
+  }
+  if (!password.match(specialChar)) {
+    throw Error("password should have at least have one special character");
+  }
+}
+
+passwordIsValid("Sizwe@13s");
+
+function passwordIsOk(password) {
+  var conditionsMet = 0;
+
+  let passwordIsOk = true;
+
+  if (password != "") {
+    conditionsMet = conditionsMet + 1;
+  }
+  if (password.match(longerThanEightChar)) {
+    conditionsMet = conditionsMet + 1;
+  }
+  if (password.match(lowercaseLetter)) {
+    conditionsMet = conditionsMet + 1;
+  }
+  if (password.match(uppercaseLetter)) {
+    conditionsMet = conditionsMet + 1;
+  }
+  if (password.match(digit)) {
+    conditionsMet = conditionsMet + 1;
+  }
+  if (password.match(specialChar)) {
+    conditionsMet = conditionsMet + 1;
+  }
+  if (conditionsMet < 3 || !password.match(longerThanEightChar)) {
+    passwordIsOk = false;
+  }
+  return passwordIsOk;
+}
+console.log(passwordIsOk("Sizweseize@13"));
+
+module.exports = { passwordIsValid, passwordIsOk };
